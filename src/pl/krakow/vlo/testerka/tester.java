9a25eb9a9945b859b00e246e13ea4169071bce69
Severus <igor.strzalka@gmail.com>
2011-12-14 17:45:55 +0100

    My head is full of fuck

--- src/pl/krakow/vlo/testerka/tester.java
+++ src/pl/krakow/vlo/testerka/tester.java
@@ -1,4 +1,12 @@
 package pl.krakow.vlo.testerka;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 /**
  * 
  */
@@ -9,6 +17,35 @@ package pl.krakow.vlo.testerka;
  *
  * TODO napisac caly projekt do testowania lol
  */
-public class tester {
-
+public class tester 
+{
+	private File root;
+	private File binary;
+	private compiler compiler;
+	private testLoader loader;
+	private resultSaver saver;
+	
+	public tester(File root)
+	{
+		super();
+		this.root = root;
+	}
+	private Process getProcess() throws IOException
+	{
+		ProcessBuilder builder = new ProcessBuilder(binary.getAbsolutePath());
+		return builder.start();
+	}
+	private void writeTest(Process task, File testcase)
+	{
+		InputStream in = new FileInputStream(testcase);
+		OutputStream out = task.getOutputStream();
+		int buff = 0;
+		while((buff = in.read())!= -1 )
+			out.write(buff);
+	}
+	public void testFile(File testCase)
+	{
+		Process task = getProcess();
+		task.
+	}
 }
